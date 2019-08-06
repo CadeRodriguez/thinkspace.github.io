fa6874ddd91b09ce72188bda469aa93d77e17777
Sarah Kogan <52415531+s-kogan@users.noreply.github.com>
2019-08-06 14:35:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,86 @@ var firebaseConfig = {
 
 var db = firebase.firestore();
 
-$("#logformm").submit(function (e) {
+
+//SIGN UP
+
+//form
+$('#signformm').submit(function(e) {
     e.preventDefault();
-console.log("Form Submitted")
+    console.log("Form submitted");
 
-    var logphone = lphone_form.value
-    var logpass = lpass_form.value
+    //variables
+    var signPhone = s_phone.value;
+    var signPass = s_pass.value;
+    var confirmPass = s_confirm.value;
 
-    saveLogToDatabase(logphone, logpass)
-})
-
-function saveLogToDatabase(logphone, logpass) {
-console.log("Executing saveLogToDatabase()...")
-    doc = db.collection("profiles").doc().set({
-       PhoneNumber: logphone,
-       Password: logpass,
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        //handle errors here
+        var errorMessage = error.message;
     })
- 
-}
+
+    verifyConfirm(signPass, confirmPass);
+
+    s_phone.value = '';
+    s_pass.value = '';
+    s_confirm.value = '';
+});
+
+//CREATE AN ACCOUNT
+
+/* 
+Steps:
+
+1. Validation inside the account - DONE
+2. 
+
+*/
+function verifyConfirm(signpass, confirmpass) {
+
+    if (signpass != confirmpass) {
+        document.getElementById('s_confirm').classList.add('shake');   
+    };
+
+
+function createID() {
+    return Math.floor(Math.random() * 8999999) + 1000000;
+};
+
+
+function choosePhoto() {
+    var photo;
+    switch (Math.floor(Math.random() * 6)) {
+        case 1:
+            photo = a;
+        case 2: 
+            photo = b;
+        case 3: 
+            photo = c;
+        case 4:
+            photo = d;
+        case 5: 
+            photo = e;
+        case 6: 
+            photo = f; 
+    };
+};
+
+
+//LOG IN
+
+$('#logformm').submit(function(e) {
+    e.preventDefault();
+    console.log("Form submitted");
+
+    var logPhone = l_phone.value;
+    var logPass = l_pass.value;
+
+    verifyLog(logPhone, logPass);
+
+    l_phone.value = '';
+    l_pass.value = '';
+});
+
+$(document).ready(function(){
+    document.getElementById('s_confirm').classList.remove('shake');
+})
