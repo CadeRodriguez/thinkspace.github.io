439bd7c2a1f6c68c1871fcc7b69596af1acd78b8
Sarah Kogan <52415531+s-kogan@users.noreply.github.com>
2019-08-08 21:26:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var firebaseConfig = {
 var db = firebase.firestore();
 
 //set variables
-var signPhone = '', signPass = '', signConfirm = '', confirmWorking, confirmText, textNumber, createdID;
+var signPhone = '', signPass = '', signConfirm = '', confirmWorking, confirmText, textNumber, createdID, logDocID;
 
 /* SmtpJS.com - v3.0.0 */
 var Email = {
@@ -47,9 +47,8 @@ var Email = {
     }
 };
 
-//SIGN UP
+//!!!SIGN UP!!!!!
 //Defining all parts of database save before actual save function
-
 
 //CONFIRM ACCOUNT
 function verifyConfirm(signpass, confirmpass) {
@@ -66,18 +65,13 @@ function verifyConfirm(signpass, confirmpass) {
 function createID() {
     createdID = Math.floor(Math.random() * 899999999) + 100000000;
     createdID = createdID.toString();
-    console.log(createdID);
 };
 
 //SEND A TEXT (set textNumber)
 function sendText(carrier){
 
     var signPhone = document.getElementById('s_phone').value;
-    console.log("confirmation: sendText");
     textNumber = signPhone.concat(carrier);
-
-    //email confirmation
-    console.log("Email will be sent to: " + textNumber);
 
     //send actual email
     Email.send({
@@ -95,15 +89,11 @@ function sendText(carrier){
 $('#signformm').submit(function(e) {
     e.preventDefault();
     
-    //confirm
-    console.log("confirmation: signform");
-
     var carrier = document.getElementById('carrier').value;
     var signPhone = document.getElementById('s_phone').value;
     var signPass = document.getElementById('s_pass').value;
     var signConfirm = document.getElementById('s_confirm').value;
 
-    console.log("Sign phone when form submitted: " + signPhone);
     //check that pass and confirm are the same
     verifyConfirm(signPass, signConfirm);
     createID();
@@ -111,20 +101,16 @@ $('#signformm').submit(function(e) {
     //if pass != confirm - shake, else send the text to carrier
     if (confirmWorking = false) {
         setTimeout(function() { document.getElementById('s_confirm').classList.remove('shake'); }, 500);
-        console.log("verifyConfirm not run");
     } else if (confirmWorking = true) {
         sendText(carrier);
-        console.log("carrier sent");
     };
 
-    console.log("textNumber: " + textNumber);
-    saveNewAccount(textNumber, signPass);
+    saveNewAccount(signPhone, signPass);
 
     console.log("confirmWorking: " + confirmWorking);
     console.log("confirmText: " + confirmText);
     if (confirmWorking && confirmText) {
-        window.location.replace("InfoPage/infoindex.html")
-        console.log('yes');
+        setTimeout(function() {window.location.replace("../InfoPage/infoindex.html"); }, 5000);
     }
 });
 
@@ -133,10 +119,44 @@ $('#signformm').submit(function(e) {
 function saveNewAccount(phone, password) {
 
     var user = createdID;
-    
-    db.collection("profiles").doc(user).set({
+
+    localStorage.setItem('userID', createdID);
+    localStorage.setItem('photo', 'https://i.ibb.co/Y4gpwF4/Artboard-20.png');
+
+    db.collection("profiles").add({
         phone: phone,
         password: password,
-        id: createdID
+        id: createdID,
+        photo: 'https://i.ibb.co/Y4gpwF4/Artboard-20.png'
     });
 };
+
+//!!!LOG IN!!!!!
+
+$('#logformm').submit(function(e) {
+    e.preventDefault();
+
+    //confirm
+    console.log("confirmation: logform");
+    var logPhone = document.getElementById('l_phone').value;
+    var logPass = document.getElementById('l_pass').value;
+    logPhoneQuery();
+
+    function logPhoneQuery() {
+        //get doc id where phone number matches
+        db.collection("profiles").where("phone", "==", logPhone).get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                logDocID = doc.id;
+                localStorage.setItem('logDocID', doc.id);
+                if (doc.data().password === logPass) {
+                    localStorage.setItem('userID', doc.data().id);
+                    window.location.replace("../Topics2/topics2.html");
+                } else if (doc.data().password != logPass) {
+                    alert('Your username and password do not match. Please try again.')
+                }
+            });
+        });
+    };
+});
+
